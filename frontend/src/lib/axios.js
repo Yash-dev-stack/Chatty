baa6bae5b4cb5204ea8fb9390ad5157a9d95f634
Yash-dev-stack <yash20050523@gmail.com>
2025-08-29 12:40:12 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://chatty-backend-96zo.onrender.com/api",
   withCredentials: true,
 });
